5965013ea46ab61f1eddf05daa96a7b8a14f5a1a
EKaxada <enocksdha@gmail.com>
2021-02-22 16:39:13 +0300

    added cors headers

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const fileRoute = require("./routes/file.routes");
+const url = "ekaxada/github.io";
 require("./db/db");
 
 const app = express();
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", url);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(fileRoute);
 
 const PORT = process.env.PORT || 5000;
