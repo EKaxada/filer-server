4847e0d106044acd17a0628b77457a34c07bb03b
Enoch Kasaadha <enocksdha@gmail.com>
2021-02-22 17:56:02 +0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
-const db = process.env.MONGO_URI || "mongodb://localhost:27017/myCloud";
+const db = "mongodb+srv://ekaxada:1234567890@cluster0.dgpl9.mongodb.net/ekaxada?retryWrites=true&w=majority" || "mongodb://localhost:27017/myCloud";
 require("mongoose")
   .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-  })
+  }).then(() => console.log("db connected..."))
+  .catch((err) => console.err(err));
