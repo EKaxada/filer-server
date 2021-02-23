867b2b0805431e3988cd8580e7f28542795ec967
EKaxada <enocksdha@gmail.com>
2021-02-23 09:44:03 +0300

    changed db settings

--- db/db.js
+++ db/db.js
@@ -1,8 +1,9 @@
-const db = "mongodb+srv://ekaxada:1234567890@cluster0.dgpl9.mongodb.net/ekaxada?retryWrites=true&w=majority" || "mongodb://localhost:27017/myCloud";
+const db = process.env.MONGO_URI || "mongodb://localhost:27017/myCloud";
 require("mongoose")
   .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-  }).then(() => console.log("db connected..."))
+  })
+  .then(() => console.log("db connected..."))
   .catch((err) => console.err(err));
